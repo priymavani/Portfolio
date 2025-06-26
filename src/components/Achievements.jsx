7961c26f6c57399ace4ac96644af757b62b2d5d0
PRIY MAVANI <priy.mavani.cg@gmail.com>
2025-06-26 15:42:22 +0530

    Add Particles component as background in Achievements section

--- src/components/Achievements.jsx
+++ src/components/Achievements.jsx
@@ -3,6 +3,7 @@ import { motion } from 'framer-motion';
 import { useInView } from 'react-intersection-observer';
 import { certificates, hackathons } from '../data';
 import SectionHeading from './ui/SectionHeading';
+import Particles from './ui/Particles'
 
 const Achievements = () => {
   const [activeCertificate, setActiveCertificate] = useState(0);
@@ -38,6 +39,9 @@ const Achievements = () => {
 
   return (
     <section id="achievements" ref={ref} className="py-20 md:py-32 bg-background-light relative">
+       <div className="absolute inset-0 z-0 pointer-events-none">
+        <Particles />
+      </div>
       <div className="absolute inset-0 overflow-hidden">
         <div className="absolute top-1/4 right-1/4 w-96 h-96 bg-primary-500/5 rounded-full filter blur-3xl"></div>
         <div className="absolute bottom-1/4 left-1/4 w-64 h-64 bg-accent-500/5 rounded-full filter blur-3xl"></div>
