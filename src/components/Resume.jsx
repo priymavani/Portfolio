9ab412e0b5e6df5265f99bc16cc80709c20f797c
PRIY MAVANI <priy.mavani.cg@gmail.com>
2025-06-26 15:42:57 +0530

    Add Threads component as background in Resume section

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { motion } from 'framer-motion';
 import { useInView } from 'react-intersection-observer';
 import SectionHeading from './ui/SectionHeading';
+import Threads from './ui/Threads'
 
 const Resume = () => {
   const [ref, inView] = useInView({
@@ -16,6 +17,10 @@ const Resume = () => {
 
   return (
     <section id="resume" ref={ref} className="py-20 md:py-32 bg-background-light relative">
+     <div className="absolute inset-0 z-0 pointer-events-none">
+
+      <Threads/>
+     </div>
       <div className="absolute inset-0 overflow-hidden">
         <div className="absolute top-1/4 right-1/4 w-96 h-96 bg-primary-500/5 rounded-full filter blur-3xl"></div>
         <div className="absolute bottom-1/4 left-1/4 w-64 h-64 bg-accent-500/5 rounded-full filter blur-3xl"></div>
