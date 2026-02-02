6e25cf585bfe2cb57ad8603a3153890667bd5242
PRIY MAVANI <priy.mavani.cg@gmail.com>
2026-02-02 20:26:29 +0530

    Update project details and add proficiency levels

--- src/data.js
+++ src/data.js
@@ -19,99 +19,161 @@ import {
   SiJira
 } from 'react-icons/si';
 import { FaCode, FaUsers, FaMobileAlt } from 'react-icons/fa';
+// NOTE: Ensure you have imported all necessary icons from 'react-icons/si' and 'react-icons/fa' 
+// corresponding to the variables used below (e.g., SiReact, SiNodedotjs, FaMobileAlt, etc.)
 
 export const projects = [
   {
     id: 0,
     title: "Task Bridge",
     description: "A full-stack task manager with role-based access (Admin & Member). Includes document sharing for seamless team collaboration.",
-    tags: ["React", "Node.js","Express", "MongoDB" , "Figma"],
+    tags: ["React", "Node.js", "Express", "MongoDB", "Figma"],
     image: "https://res.cloudinary.com/dd6lqkak0/image/upload/v1740054324/team_%22team%40123%22/fkq15ai5cvm22xwgtjzj.png",
     link: "https://task-bridge-project-managment.netlify.app/",
     github: "https://github.com/priymavani/task_bridge",
-    figma : "https://www.figma.com/design/jEl4fmd7xKQj1auwksj5Q1/TASK-BRIDGE?node-id=0-1&t=iC8RW9DaJqFLbqNL-1",
-    featured: true
+    figma: "https://www.figma.com/design/jEl4fmd7xKQj1auwksj5Q1/TASK-BRIDGE?node-id=0-1&t=iC8RW9DaJqFLbqNL-1",
+    featured: true,
+    demo: 'Live Demo',
+    code: 'View Code',
+    video: "https://www.youtube.com/watch?v=KXHnwewU73w"
   },
   {
     id: 1,
-    title: " Farm Trust ",
-    description: "FarmTrust is a MERN stack platform connecting consumers with certified organic farmers. It features product browsing, farmer profiles, AI chatbot support, and tools for farmers to manage sales and certifications.",
-    tags: ["React", "Node.js","Express", "MongoDB","Figma"],
-    image: "https://res.cloudinary.com/dd6lqkak0/image/upload/v1748109164/farmtrust.netlify.app__1_z9feph.png",
-    figma : "https://www.figma.com/design/SNuN8wbnReIue8nUOJhjEG/Untitled?node-id=0-1&p=f&t=6MhTQoFKTuSxnhJE-0",
-    link: "https://farmtrust.netlify.app/",
-    github: "https://github.com/priymavani/FarmTrust-x-Hackathon",
-    featured: true
+    title: "Stock-Master",
+    description: "StockMaster is an inventory management system that streamlines stock operations across multiple locations. It enables real-time inventory tracking, secure stock movement management, and detailed audit trails.",
+    tags: ["react", "node", "express", "mongodb"],
+    image: "https://res.cloudinary.com/dd6lqkak0/image/upload/v1768652846/projects/jj789jdbvxhew8rceobj.png",
+    link: "https://odoo-stock-frontend.vercel.app/",
+    github: "https://github.com/priymavani/Odoo_StockMaster",
+    featured: true,
+    demo: 'Live Demo',
+    code: 'View Code'
   },
   {
     id: 2,
-    title: "Coding Ninjas Clone",
-    description: "Developed a static homepage clone of Coding Ninjas with structured UI elements and styling.",
-    tags: ["HTML", "CSS", ],
-    image: "https://res.cloudinary.com/dd6lqkak0/image/upload/v1740054348/team_%22team%40123%22/ysx1a2ghjb0trdd3zk9u.png",
-    github: "https://github.com/priymavani/CODING_NINJAS_project",
-    link: "https://cogingninjasbypriymavani.netlify.app/",
-    featured: true
+    title: "Book Ease",
+    description: "Appointment Booking System is a role-based platform that simplifies scheduling through real-time availability, intelligent slot management, and interactive calendars.",
+    tags: ["react", "node", "mongodb", "express", "jwt", "MCP server"],
+    image: "https://res.cloudinary.com/dd6lqkak0/image/upload/v1768652393/projects/aacrm6tehmo4l5tgzwbk.png",
+    link: "https://odoo-spit-appointment-app-nexbyte.vercel.app/",
+    github: "https://github.com/priymavani/Odoo_SPIT_Appointment_app_NexByte",
+    postman: "https://documenter.getpostman.com/view/39217138/2sB3dWqmfM",
+    featured: true,
+    demo: 'Postman Documentation',
+    code: 'View Code'
   },
   {
     id: 3,
-    title: "LinkedIn API",
-    description: "RESTful API using Express.js/MongoDB for professional networking platform clone. Handles core features: user profiles, connections, posts, messaging, and premium account management.",
-    tags: ["Node.js", "Mongodb", "Express"],
-    image: "https://res.cloudinary.com/dd6lqkak0/image/upload/v1740054368/team_%22team%40123%22/ptftaalkv1ljxwjmyk6h.png",
-    postman: "https://documenter.getpostman.com/view/39217138/2sAYQUpuAq",
-    github: "https://github.com/priymavani/APIs/tree/main/linkedin_API",
-    link: "https://documenter.getpostman.com/view/39217138/2sAYQUpuAq",
-    featured: true
+    title: "Farm Trust",
+    description: "FarmTrust is a MERN stack platform connecting consumers with certified organic farmers. It features product browsing, farmer profiles, AI chatbot support, and tools for farmers to manage sales and certifications.",
+    tags: ["React", "Node.js", "Express", "MongoDB", "Figma"],
+    image: "https://res.cloudinary.com/dd6lqkak0/image/upload/v1748109164/farmtrust.netlify.app__1_z9feph.png",
+    link: "https://farmtrust.netlify.app/",
+    github: "https://github.com/priymavani/FarmTrust-x-Hackathon",
+    figma: "https://www.figma.com/design/SNuN8wbnReIue8nUOJhjEG/Untitled?node-id=0-1&p=f&t=6MhTQoFKTuSxnhJE-0",
+    featured: true,
+    demo: 'Live Demo',
+    code: 'View Code'
   },
-
   {
     id: 4,
-    demo:'Live Demo',
-    code:'View Code',
-    title: ' Realme Clone',
-    description: 'Built a static clone of Realme’s homepage, accurately matching its layout and content.',
-    tags: ['HTML', 'CSS'],
-    image: 'https://res.cloudinary.com/dd6lqkak0/image/upload/v1748109074/realmebypriymavani.netlify.app__qsxabg.png',
-    link: 'https://realmebypriymavani.netlify.app/',
-    github: 'https://github.com/priymavani/realme_clone',
-    featured: true
+    title: "Food Share",
+    description: "Food Share is a food donation matching platform that connects restaurants and food providers with nearby NGOs to reduce waste and fight hunger. It enables real-time location-based matching.",
+    tags: ["react", "node", "express", "mongodb"],
+    image: "https://res.cloudinary.com/dd6lqkak0/image/upload/v1768652652/projects/uq94vccme7vq1xzcpzcr.png",
+    link: "",
+    github: "https://github.com/priymavani/food-share",
+    featured: true,
+    demo: 'Live Demo',
+    code: 'View Code'
   },
   {
     id: 5,
-    demo:'Postman Documentation',
-    code:'View Code',
-    title: 'LinkedIn API',
-    description: ' RESTful API using Express.js/MongoDB for professional networking platform clone Handles core features: user profiles, connections, posts, messaging, and premium account management',
-    tags: ['Node.js', 'MongoDB','Postman'],
-    image: "https://res.cloudinary.com/dd6lqkak0/image/upload/v1740054368/team_%22team%40123%22/ptftaalkv1ljxwjmyk6h.png",
-    link: 'https://documenter.getpostman.com/view/39217138/2sAYQUpuAq',
-    github: 'https://github.com/priymavani/APIs/tree/main/linkedin_API',
-    featured: true
+    title: "LinkedIn Text Formatter",
+    description: "LinkedIn Text Formatter is a browser extension that enhances LinkedIn posts, messages, and comments with rich text styling. It allows users to apply visually appealing text formats and structured bullets.",
+    tags: ["html", "css", "extention"],
+    image: "https://res.cloudinary.com/dd6lqkak0/image/upload/v1768653156/projects/idk9dc0jwywsjp6a5kmd.png",
+    link: "",
+    github: "https://github.com/priymavani/Extension-LinkedIn-Text-Formatter",
+    featured: true,
+    demo: 'Live Demo',
+    code: 'View Code',
+    video: "https://youtu.be/u4klNTJ6ixY"
   },
   {
     id: 6,
-    demo:'Postman Documentation',
-    code:'View Code',
-    title: 'YouTube API',
-    description: '  Built RESTful API for YouTube-clone using Node.js/MongoDB Managed videos, users, comments, and subscriptions with CRUD operations',
-    tags: ['Node.js', 'MongoDB','Postman'],
-    image: 'https://res.cloudinary.com/dd6lqkak0/image/upload/v1740054378/team_%22team%40123%22/slqe466glrqs5gzxrxeo.png',
-    link: 'https://documenter.getpostman.com/view/39217138/2sAYXEDd33',
-    github: 'https://github.com/priymavani/APIs/tree/main/YouTube_API',
-    featured: true
+    title: "React Project",
+    description: "React application integrating multiple public APIs (Harry Potter, Cocktails, Meals, Banking) with search functionality. Component-based architecture using React Router and Hooks for state management.",
+    tags: ["React", "API"],
+    image: "https://res.cloudinary.com/dd6lqkak0/image/upload/v1740054356/team_%22team%40123%22/qs7ndf4qekv5fkothlab.png",
+    link: "https://reactapitask.netlify.app/",
+    github: "https://github.com/priymavani/React-App",
+    featured: true,
+    demo: 'Live Demo',
+    code: 'View Code'
   },
   {
     id: 7,
-    demo:'Live Demo',
-    code:'View Code',
-    title: ' React Project',
-    description: '   React application integrating multiple public APIs (Harry Potter, Cocktails, Meals, Banking) with search functionality Component-based architecture using React Router and Hooks for state management',
-    tags: ['React', 'API'],
-    image: 'https://res.cloudinary.com/dd6lqkak0/image/upload/v1740054356/team_%22team%40123%22/qs7ndf4qekv5fkothlab.png',
-    link: 'https://reactapitask.netlify.app/',
-    github: 'https://github.com/priymavani/React-App',
-    featured: true
+    title: "Realme Clone",
+    description: "Built a static clone of Realme's homepage, accurately matching its layout and content.",
+    tags: ["HTML", "CSS"],
+    image: "https://res.cloudinary.com/dd6lqkak0/image/upload/v1748109074/realmebypriymavani.netlify.app__qsxabg.png",
+    link: "https://realmebypriymavani.netlify.app/",
+    github: "https://github.com/priymavani/realme_clone",
+    featured: true,
+    demo: 'Live Demo',
+    code: 'View Code'
+  },
+  {
+    id: 8,
+    title: "Coding Ninjas Clone",
+    description: "Developed a static homepage clone of Coding Ninjas with structured UI elements and styling.",
+    tags: ["HTML", "CSS"],
+    image: "https://res.cloudinary.com/dd6lqkak0/image/upload/v1740054348/team_%22team%40123%22/ysx1a2ghjb0trdd3zk9u.png",
+    link: "https://cogingninjasbypriymavani.netlify.app/",
+    github: "https://github.com/priymavani/CODING_NINJAS_project",
+    featured: true,
+    demo: 'Live Demo',
+    code: 'View Code'
+  },
+  {
+    id: 9,
+    title: "LinkedIn API",
+    description: "RESTful API using Express.js/MongoDB for professional networking platform clone. Handles core features: user profiles, connections, posts, messaging, and premium account management.",
+    tags: ["Node.js", "MongoDB", "Express"],
+    image: "https://res.cloudinary.com/dd6lqkak0/image/upload/v1740054368/team_%22team%40123%22/ptftaalkv1ljxwjmyk6h.png",
+    link: "https://documenter.getpostman.com/view/39217138/2sAYQUpuAq",
+    github: "https://github.com/priymavani/APIs/tree/main/linkedin_API",
+    postman: "https://documenter.getpostman.com/view/39217138/2sAYQUpuAq",
+    featured: true,
+    demo: 'Postman Documentation',
+    code: 'View Code'
+  },
+  {
+    id: 10,
+    title: "YouTube-Inspired API",
+    description: "Built RESTful API for YouTube-clone using Node.js/MongoDB Managed videos, users, comments, and subscriptions with CRUD operations",
+    tags: ["node", "express", "mongodb"],
+    image: "https://res.cloudinary.com/dd6lqkak0/image/upload/v1740054378/team_%22team%40123%22/slqe466glrqs5gzxrxeo.png",
+    link: "",
+    github: "https://github.com/priymavani/APIs/tree/main/YouTube_API",
+    postman: "https://documenter.getpostman.com/view/39217138/2sAYXEDd33",
+    featured: true,
+    demo: 'Postman Documentation',
+    code: 'View Code'
+  },
+  {
+    id: 11,
+    title: "poll_vibe - open-source Contribution",
+    description: "Engineering the backend infrastructure for \"Image Polls,\" enabling users to seamlessly upload and attach rich media to voting options. Integrated Cloudinary CDN.",
+    tags: ["nodejs", "express", "Cloudinary"],
+    image: "https://res.cloudinary.com/dd6lqkak0/image/upload/v1770041086/projects/txk4chnlurtb2zqm0hgs.png",
+    link: "",
+    github: "https://github.com/codinggita/poll_vibe/pull/10",
+    video: "https://github.com/codinggita/poll_vibe/pull/10",
+    featured: false,
+    demo: 'Live Demo',
+    code: 'View Code'
   }
 ];
 
@@ -119,42 +181,40 @@ export const skills = [
   {
     category: "Frontend",
     items: [
-      { name: "React", icon: SiReact },
-      { name: "Bootstrap", icon: SiBootstrap },
-      { name: "HTML", icon: SiHtml5 },
-      { name: "CSS", icon: SiCss3 },
-      { name: "JavaScript", icon: SiJavascript },
-      { name: "Tailwind CSS", icon: SiTailwindcss },
-      { name: "Responsive Design", icon: FaMobileAlt }
+      { name: "React", icon: SiReact, proficiency: 85 },
+      { name: "Bootstrap", icon: SiBootstrap, proficiency: 80 },
+      { name: "HTML", icon: SiHtml5, proficiency: 90 },
+      { name: "CSS", icon: SiCss3, proficiency: 85 },
+      { name: "JavaScript", icon: SiJavascript, proficiency: 85 },
+      { name: "Tailwind CSS", icon: SiTailwindcss, proficiency: 80 },
+      { name: "Responsive Design", icon: FaMobileAlt, proficiency: 85 }
     ]
   },
   {
     category: "Backend",
     items: [
-      { name: "Node.js", icon: SiNodedotjs },
-      { name: "Express.js", icon: SiExpress },
-      { name: "MongoDB", icon: SiMongodb },
-      { name: "REST API", icon: SiPostman },
-      // { name: "Firebase", icon: SiFirebase }
+      { name: "Node.js", icon: SiNodedotjs, proficiency: 80 },
+      { name: "Express.js", icon: SiExpress, proficiency: 80 },
+      { name: "MongoDB", icon: SiMongodb, proficiency: 75 },
+      { name: "REST API", icon: SiPostman, proficiency: 85 }
     ]
   },
   {
     category: "Tools & Technologies",
     items: [
-      { name: "Git", icon: SiGit },
-      { name: "GitHub", icon: SiGithub },
-      { name: "Postman", icon: SiPostman },
-      { name: "Netlify", icon: SiNetlify },
-      { name: "Vercel", icon: SiVercel }
+      { name: "Git", icon: SiGit, proficiency: 80 },
+      { name: "GitHub", icon: SiGithub, proficiency: 85 },
+      { name: "Postman", icon: SiPostman, proficiency: 85 },
+      { name: "Netlify", icon: SiNetlify, proficiency: 75 },
+      { name: "Vercel", icon: SiVercel, proficiency: 75 }
     ]
   },
   {
     category: "Other Skills",
     items: [
-      { name: "UI/UX Design", icon: SiFigma },
-      { name: "Problem Solving", icon: FaCode },
-      { name: "Team Collaboration", icon: FaUsers },
-      // { name: "Agile Methodologies", icon: SiJira }
+      { name: "UI/UX Design", icon: SiFigma, proficiency: 70 },
+      { name: "Problem Solving", icon: FaCode, proficiency: 85 },
+      { name: "Team Collaboration", icon: FaUsers, proficiency: 90 }
     ]
   }
 ];
@@ -204,18 +264,18 @@ export const certificates = [
     title: 'Introduction to C',
     issuer: 'SoloLearn',
     credentialId: "CC-RAJXJIZY",
-    image:'https://res.cloudinary.com/dd6lqkak0/image/upload/v1740051851/team_%22team%40123%22/oehlrhcleacbhphf7qph.png',
-    credentialUrl :'https://www.sololearn.com/certificates/CC-RAJXJIZY'
+    image: 'https://res.cloudinary.com/dd6lqkak0/image/upload/v1740051851/team_%22team%40123%22/oehlrhcleacbhphf7qph.png',
+    credentialUrl: 'https://www.sololearn.com/certificates/CC-RAJXJIZY'
   },
   {
     id: 4,
     title: 'Introduction to JavaScript',
     issuer: 'Sololearn',
     credentialId: "CC-JWTVNWRM",
-    image:'https://res.cloudinary.com/dd6lqkak0/image/upload/v1740051857/team_%22team%40123%22/ocvk8wn6ddvumh8qndfy.png',
-    credentialUrl : 'https://www.sololearn.com/certificates/CC-JWTVNWRM'
+    image: 'https://res.cloudinary.com/dd6lqkak0/image/upload/v1740051857/team_%22team%40123%22/ocvk8wn6ddvumh8qndfy.png',
+    credentialUrl: 'https://www.sololearn.com/certificates/CC-JWTVNWRM'
   },
-    {
+  {
     id: 5,
     title: "JavaScript (Basic)",
     issuer: "HackerRank",
@@ -244,29 +304,58 @@ export const certificates = [
 export const hackathons = [
   {
     id: 1,
-    title: "FarmTrust Hackathon",
-    description: "Developed a MERN stack platform connecting consumers with certified organic farmers. Implemented features like product browsing, farmer profiles, AI chatbot support, and tools for farmers to manage sales and certifications.",
-    image: "https://res.cloudinary.com/dd6lqkak0/image/upload/v1748109164/farmtrust.netlify.app__1_z9feph.png",
+    title: "Odoo x Spit",
+    description: "Finalist at Odoo x Spit hackathon. Developed 'Book Ease' - An Appointment Booking System using React, Node, and MongoDB.",
+    image: "https://res.cloudinary.com/dd6lqkak0/image/upload/v1768733209/hackathons/fj21s7mgfgtyc6dgfvhp.jpg",
     teamMembers: [
-      {
-        name: "Priy Mavani",
-        role: "Frontend Developer",
-        // image: "https://res.cloudinary.com/dd6lqkak0/image/upload/v1740054324/team/priya.jpg"
-      },
-      {
-        name: "Jadav Parth",
-        role: "Backend Developer",
-        // image: "https://res.cloudinary.com/dd6lqkak0/image/upload/v1740054324/team/member2.jpg"
-      },
-      {
-        name: "Dhruv Sonagra",
-        role: "Frontend Developer",
-        // image: "https://res.cloudinary.com/dd6lqkak0/image/upload/v1740054324/team/member3.jpg"
-      }
+      { name: "Priy Mavani", role: "Backend developer" },
+      { name: "Mayur Waykar", role: "backend Developer" },
+      { name: "Krish Shyara", role: "Frontend Developer" },
+      { name: "Vanshika Jangam", role: "Frontend Developer" }
+    ],
+    demoLink: "",
+    githubLink: "https://github.com/priymavani/Odoo_SPIT_Appointment_app_NexByte"
+  },
+  {
+    id: 2,
+    title: "Odoo X Gujarat Vidyapith Hackathon",
+    description: "Participated in Odoo X Gujarat Vidyapith Hackathon. Worked on 'Farm Trust', a platform connecting consumers with organic farmers.",
+    image: "https://res.cloudinary.com/dd6lqkak0/image/upload/v1770042216/hackathons/zdqezcqhx5ildatwcaeq.jpg",
+    teamMembers: [
+      { name: "Priy Mavani", role: "Frontend Developer" },
+      { name: "Parth Jadav", role: "Backend Developer" },
+      { name: "Dhruv Sonagra", role: "Frontend Developer" }
     ],
     demoLink: "https://farmtrust.netlify.app/",
-    githubLink: "https://github.com/priymavani/FarmTrust-x-Hackathon",
-    figmaLink: "https://www.figma.com/design/SNuN8wbnReIue8nUOJhjEG/Untitled?node-id=0-1&p=f&t=6MhTQoFKTuSxnhJE-0"
+    githubLink: "https://github.com/priymavani/FarmTrust-x-Hackathon"
+  },
+  {
+    id: 3,
+    title: "Nexothon GCET",
+    description: "Participated in Nexothon GCET. Developed 'Design2Code' project with a team of four developers.",
+    image: "https://res.cloudinary.com/dd6lqkak0/image/upload/v1770042763/hackathons/ex84qoteavgqvbortjqs.jpg",
+    teamMembers: [
+      { name: "Priy Mavani", role: "Backend Developer" },
+      { name: "Vanshika jangam", role: "Frontend Developer" },
+      { name: "Krish Shyara", role: "Frontend Developer" },
+      { name: "Dhruvesh Shyara", role: "Backend Developer" }
+    ],
+    demoLink: "",
+    githubLink: "https://github.com/priymavani/Design2Code"
+  },
+  {
+    id: 4,
+    title: "Rai University State Level Hackthon",
+    description: "Participated in the State Level Hackathon at Rai University. Contributed to the 'Food Share' project.",
+    image: "https://res.cloudinary.com/dd6lqkak0/image/upload/v1770043772/hackathons/zujdauk3wihhefrn42bm.jpg",
+    teamMembers: [
+      { name: "Priy Mavani", role: "Backend Developer" },
+      { name: "Priyasha Yadav", role: "Backend Developer" },
+      { name: "Krish Shyara", role: "Frontend Developer" },
+      { name: "Dhruvesh Shyara", role: "Frontend Developer" },
+      { name: "Vanshika Jangam", role: "Backend Developer" }
+    ],
+    demoLink: "",
+    githubLink: "https://github.com/priymavani/food-share"
   }
 ];
-
